509a3c33d7568e4ffaf91d395186ee7055a03218
Yu Mentsel <yu.mentsel@gmail.com>
2023-10-22 23:42:25 +0300

    feat: generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { ProfileComponent } from './header/profile/profile.component';
+import { SearchFormComponent } from './header/search-form/search-form.component';
+import { SortFormComponent } from './header/sort-form/sort-form.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent,
+    SearchFormComponent,
+    SortFormComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/header/sort-form/sort-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-form',
+  templateUrl: './sort-form.component.html',
+  styleUrls: ['./sort-form.component.scss'],
+})
+export class SortFormComponent {}
