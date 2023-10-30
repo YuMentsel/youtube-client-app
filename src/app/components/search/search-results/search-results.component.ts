8627e55da8b00d60090c33abd106307ff28e264a
Yu Mentsel <yu.mentsel@gmail.com>
2023-10-30 03:12:47 +0300

    feat: add search results

--- src/app/components/search/search-results/search-results.component.ts
+++ src/app/components/search/search-results/search-results.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { SearchItem } from '../../../models/search-item.model';
+import { response } from '../../../data/response';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
 })
-export class SearchResultsComponent {}
+export class SearchResultsComponent {
+  searchItems: SearchItem[] = response.items;
+}
