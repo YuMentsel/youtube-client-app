68330322e113c2b6462918e982258ffa8ae77fc9
Yu Mentsel <yu.mentsel@gmail.com>
2023-11-26 21:38:16 +0300

    feat: migrate app to jest

--- src/app/core/pipes/sort-by-value.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SortByValuePipe } from './sort-by-value.pipe';
-
-describe('SortByValuePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SortByValuePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
